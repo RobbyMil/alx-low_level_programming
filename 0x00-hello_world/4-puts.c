da6a4b4510e2f052cf821b0d20653c422d4e0726
Robby Milambo <robbymilambo@gmail.com>
2022-06-30 14:48:56 -0700

    c program to print using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,8 @@
-#!/bin/bash
-puts "Programming is like building a multilingual puzzle"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
+}

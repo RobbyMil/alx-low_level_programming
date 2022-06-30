b1ad51100809c19e0cb14ba2be34a164f634be67
Robby Milambo <robbymilambo@gmail.com>
2022-06-30 15:27:24 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
- *Main function is the entry point
+/**
+ *Main- is the entry point
  *All other functions are called from within it.
- *return when return is equal to zero it means the function
+ *When return is equal to zero it means the function
  *executed successfully.
-*/
+**/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");

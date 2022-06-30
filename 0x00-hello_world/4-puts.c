016e68992c81a8c19465b33429e1f4087726906a
Robby Milambo <robbymilambo@gmail.com>
2022-06-30 15:07:39 -0700

    print with puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*Main is the entry point
+ *All other functions are called from within it.
+ *return when return is equal to 0 it means tue function
+ *executed successfullt
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }

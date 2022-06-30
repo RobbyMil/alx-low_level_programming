9c21c2b3d58f0a73dd2d5360bcb54f226347c6ca
Robby Milambo <robbymilambo@gmail.com>
2022-06-30 15:16:40 -0700

    print with puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*Main is the entry point
+/*
+ *Main function is the entry point
  *All other functions are called from within it.
- *return when return is equal to 0 it means tue function
- *executed successfullt
+ *return when return is equal to zero it means the function
+ *executed successfully.
 */
 int main(void)
 {

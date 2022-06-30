cf5e2f0a3d41d42f0ab9e45a26d669532e731a15
Robby Milambo <robbymilambo@gmail.com>
2022-06-30 16:49:46 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - is the entry pointh
+ * rerurn: always 0 success
+*/
+
+int main (void)
+{
+	printf("with proper grammar, but the outcome is a piece 9f art,\n");
+	return (0):
+}

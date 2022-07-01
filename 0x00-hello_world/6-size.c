ea4682ac499b3c08cccd49a480e81d0f54b913db
Robby Milambo <robbymilambo@gmail.com>
2022-06-30 17:18:35 -0700

    dd

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,9 +5,10 @@
   *Return: Always 0 means (Success) 
   */ 
   
- int main(void) 
- { 
-         printf("Size of a char: %zu byte(s)\n", sizeof(char)); 
+int main(void) 
+
+{ 
+	printf("Size of a char:%zubyte(s)\n",sizeof(char)); 
          printf("Size of an int: %zu byte(s)\n", sizeof(int)); 
          printf("Size of a long int: %zu byte(s)\n", sizeof(long int)); 
          printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int)); 

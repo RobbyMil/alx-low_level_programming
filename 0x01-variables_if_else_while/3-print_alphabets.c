5262cf64c3d1f3198ad805f5b98047b8742edbe8
Robby Milambo <robbymilambo@gmail.com>
2022-07-02 17:14:27 +0200

    print alphabets in lower and then upper

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints all alphabets letters in lower cases then in upper cases then  newline at the end
+ * Return: returns value 0 to signify success
+*/
+
+int main(void)
+{
+	char lowers;
+
+	char uppers;
+
+	lowers = 'a';
+	
+	uppers = 'A';
+
+	while (lowers <= 'z' && uppers <= 'Z')
+	{
+		while (lowers <= 'z')
+		{
+			putchar(lowers);
+			lowers++;
+		}
+		while (uppers <= 'Z')
+		{
+			putchar(uppers);
+			uppers++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}

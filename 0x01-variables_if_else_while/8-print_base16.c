79c8b7a1a0e58e22b86299721e94341d98128f8f
Robby Milambo <robbymilambo@gmail.com>
2022-07-03 09:27:59 +0200

    print base 16 digits and ASCII chars

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - prints all num and letters in base 16
+ *used nested while loop to verify two conditions
+ *used ASCII table to map Dec values matching char in var digit
+*/
+
+int main(void)
+{
+	char letter = 97;
+
+	int digit = 48;
+
+	while (digit <= 57 && letter <= 102)
+	{
+		while (digit <= 57)
+		{
+			putchar(digit);
+			digit++;
+		}
+		while (letter <= 102)
+		{
+			putchar(letter);
+			letter++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}

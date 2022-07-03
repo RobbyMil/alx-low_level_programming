ba9c93029b3767ae7dce701a2111047d18c33112
Robby Milambo <robbymilambo@gmail.com>
2022-07-03 11:16:48 +0200

    sizes of data types using sizeof()

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,25 @@
-#include<stdio.h> 
-  
- /** 
-  *main - Entry point 
-  *Return: Always 0 means (Success) 
-  */ 
-  
-int main(void) 
+#include <stdio.h>
+#include <stdlib.h>
 
-{ 
-	 printf("Size of a char: %lu byte(s)\n",sizeof(char)); 
-         printf("Size of an int: %lu byte(s)\n",sizeof(int)); 
-         printf("Size of a long int: %lu byte(s)\n",sizeof(long int)); 
-         printf("Size of a long long int: %lu byte(s)\n",sizeof(long long int)); 
-         printf("Size of a float: %lu byte(s)\n",sizeof(float)); 
-         return (0); 
- }
+/**
+ *main - prints the sizes of each data types in C using printf and the appropriate format identifier
+ *used sizeof() function to read size of variables
+ *'$?' command shows the exit code of a program
+ *Return: returns value 0 to signify success of operation
+*/
+
+int nain(void)
+{
+	int a;
+	char b;
+	float c;
+	long int d;
+	long long int e;
+
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %c byte(s)\n", sizeof(int));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lld\n", sizeof(long long));
+	printf("Size of a float: %f byte(s)\n", sizeof(float)
+	return (0);
+}

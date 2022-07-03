3961e63b3a7572d1257096613d38b68351ae714a
Robby Milambo <robbymilambo@gmail.com>
2022-07-03 07:12:39 +0200

    quick fix

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,16 +5,16 @@
  * main - prints single digits of base 10 starting from 0
  * used ASCII table to map DEC values to char from 0-9
  * DEC values from 48 to 57 matches digits 0-9
- * declared and initialized int variable num with 47
+ * declared and initialized int variable num with 48
  * used while loop to putchar num and increment it +1 untilnum == 57
  * Return: returns value 0 for successful operation
 **/
 
 int main(void)
 {
-	int num = 47;
-	
-	while (num<= 58)
+	int num = 48;
+
+	while (num <= 57)
 	{
 		putchar(num);
 		num++;

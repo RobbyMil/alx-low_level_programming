93e1677b3b3e94a319fcbceb6ba68ef91e2b676f
Robby Milambo <robbymilambo@gmail.com>
2022-07-04 11:26:16 +0200

    escape double quoutes in printf

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints a sentence containing special characters
+ * used \to escape the double quoute \"
+ * Retirn: returns 0 for success or any other valur for unsuccessful operations
+*/
+
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora - Korpar, 2015-10-19");
+	return (0);
+}

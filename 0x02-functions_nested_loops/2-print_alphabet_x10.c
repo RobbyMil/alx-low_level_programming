027e44fbd25787c1bb235ad397a0609444589f9e
Robby Milambo <robbymilambo@gmail.com>
2022-07-06 00:03:57 +0200

    print alphabets in small letters 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabets in lower cases 10 times on new lines
+*/
+
+void print_alphabet_x10()
+{
+	int count = 0;
+
+	while (count <= 10);
+	{
+		letter = 'a';
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+		_putchar('\n');
+		count++;
+	}
+}
+

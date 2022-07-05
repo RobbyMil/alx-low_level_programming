80165ec7051618daa1eedb140fbdf9b462b440ca
Robby Milambo <robbymilambo@gmail.com>
2022-07-05 21:48:57 +0200

    prints putchar_ on console

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print 'putchar_' on console followed by new line
+ *Return: returns 0 for successful operation
+*/
+
+int main(voi)
+{
+	printf("putchar_\n");
+	return (0);
+}

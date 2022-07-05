512c358c149791fe2e9cdf6905dbd27ef84c41c9
Robby Milambo <robbymilambo@gmail.com>
2022-07-05 22:26:28 +0200

    function to print alphabet in lower letters

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet in lower cases with new line
+ * Return: return value 0 for successful operation
+*/
+
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+	return (0);
+}

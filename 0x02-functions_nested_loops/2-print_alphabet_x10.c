4640a67146b9eee70606eb03a403fb8020b8327b
Robby Milambo <robbymilambo@gmail.com>
2022-07-06 00:19:25 +0200

    hotfix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,12 +4,12 @@
  * print_alphabet_x10 - prints alphabets in lower cases 10 times on new lines
 */
 
-void print_alphabet_x10()
+void print_alphabet_x10(void)
 {
 	int count = 0;
 	char letter;
 
-	while (count <= 10)
+	while (count < 10)
 	{
 		letter = 'a';
 		while (letter <= 'z')

b7ba62fb8441162dbd3e760a31734b3e01c268b3
Robby Milambo <robbymilambo@gmail.com>
2022-07-07 00:08:51 +0200

    hotfix

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,14 +7,13 @@
 */
 
 int print_last_digit(int n)
-{ 
-
+{
 	if (n < 0)
 	{
-		n*=-1;
+		n*= - 1;
 
-		return (n % 10);
+		return ((n % 10) * (11));
 	}
 	else
-		return (n % 10);
+		return ((n % 10) * (11));
 }

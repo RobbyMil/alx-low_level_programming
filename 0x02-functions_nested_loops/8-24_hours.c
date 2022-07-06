f046d319cc9a25f2ce1e9931d281f93225c002cd
Robby Milambo <robbymilambo@gmail.com>
2022-07-07 01:08:15 +0200

    hotfix

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,15 +14,14 @@ void jack_bauer(void)
 	 {
 		 while (min < 60)
 		 {
-			 _putchar('0' + (hours / 10));
-			 _putchar('0' + (hours % 10));
+			 _putchar('0' + (hours / 10));
+			 _putchar('0' + (hours % 10));
 			 _putchar(':');
-			 _putchar('0' + (min / 10));
-			 _putchar('0' + (min % 10));
-			 _putchar('\n');
+			 _putchar('0' + (min / 10));
+			 _putchar('0' + (min % 10));
 			 min++;
 		 }
-		 min = 0;
+		 min = 0;
 		 hours++;
 	 }
  }

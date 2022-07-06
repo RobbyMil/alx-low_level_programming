9261cededf1c303c5cf4d40164bc1aea72b7cf1c
Robby Milambo <robbymilambo@gmail.com>
2022-07-07 00:53:42 +0200

    hot fix

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,19 +1,18 @@
-#include "main.h" 
-  
- /** 
-  * jack_bauer - print every minute of a day 
-  * Description: print minutes 00:00 to 23:59 
-  */ 
-  
- void jack_bauer(void) 
- {
-	 int hours = 0;
-	 
-	 int min = 0;
+# include "main.h"
 
-	 while (hours < 24)
+/**
+ * jack_bauer - prints every minute of the day
+*/
+
+void jack_bauer(void)
+ {
+	 int hours = 0;
+
+	 int min = 0;
+
+	 while (hours < 24)
 	 {
-		 while (min < 60)
+		 while (min < 60)
 		 {
 			 _putchar('0' + (hours / 10));
 			 _putchar('0' + (hours % 10));

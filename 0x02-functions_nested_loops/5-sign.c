eee5e21ded0276c134d1ec96ba55683d5d452770
Robby Milambo <robbymilambo@gmail.com>
2022-07-06 08:35:25 +0200

    header file fixes

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,28 +2,27 @@
 
 /**
  * print_sign - checks if argument is positive or negative
- * @c: argument to be checked
+ * @n: argument to be checked
  * Return: if c is < 0 returns 1 and prints +
  * if c is 0 returns 0 and prints 0
  * if c is < 0 returns -1 and prints -
 */
 
-int print_sign(int c)
+int print_sign(int n)
 {
-	if (c > 0)
+	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
 	}
-	else if (c == 0)
+	else if (n == 0)
 	{
 		_putchar('0');
 		return (0);
 	}
-	else if (c < 0)
+	else if (n < 0)
 	{
 		_putchar('-');
 		return (-1);
 	}
-	_putchar('\n');
 }

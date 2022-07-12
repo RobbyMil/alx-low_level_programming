316fdfb920b5bbe35fc48ee1f0fc7ccfa2a6ed58
Robby Milambo <robbymilambo@gmail.com>
2022-07-12 06:03:49 +0200

    rev string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,37 @@
+# include "main.h"
+
+/**
+ * _strlen - compute the lenght of a stting
+ * @s: argument string
+ * Return: lenght of string s
+*/
+
+int _strlenght(*char s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+}
+
+void rev_string(char *s)
+{
+	int i = 0;
+
+	int j = (_strlen(-s) - 1);
+
+	char swp;
+
+	while (i < j)
+	{
+		swp = s[i];
+		s[i] = s[j];
+		s[j] = swp;
+		i++, j--;
+	}
+}

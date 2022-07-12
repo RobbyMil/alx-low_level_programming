275e4575db66c0ae3d62def8126e503519ee9093
Robby Milambo <robbymilambo@gmail.com>
2022-07-12 04:46:11 +0200

    reverse string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,29 @@
+# include "main.h"
+
+/**
+ *print_rev - reverse a string and prints it
+ @s: argument string to be reversed
+*/
+
+void print_rev(char *s)
+{
+	int len = 0;
+
+	int i;
+
+	while (*s != '\0')
+	{
+		len++;
+		++s;
+	}
+
+	s--;
+	for (i = len; i>0; i--)
+	{
+		putchar(*s);
+		s--;
+	}
+	
+	putchar('\n');
+}
+

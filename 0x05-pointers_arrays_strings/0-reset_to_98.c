6920b7e3deed8ac78129228c0f7d47dba8082cee
Robby Milambo <robbymilambo@gmail.com>
2022-07-12 03:55:42 +0200

    reset to 98 pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+# include "main.h"
+
+/**
+ * reset_to_98 - receive a parameter pointer to an int
+ * and change its value to 98
+ * @n: pointer to int n
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Exercises and solutions related to pointers, arrays and strings in C

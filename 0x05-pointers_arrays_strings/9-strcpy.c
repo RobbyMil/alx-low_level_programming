aa3c378be7f1f3f003945c3f61bf972ff48688b8
Robby Milambo <robbymilambo@gmail.com>
2022-07-13 05:37:45 +0200

    fix

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,15 +8,22 @@
 
 int _strlen(char *s)
 {
-        int len = 0;
+	int len = 0;
 
-        while (*s != '\0')/*'\0' is empty string character*/
-        {
-                len++;
-                s++;
-        }
-        return (len);
+	while (*s != '\0')/*'\0' is empty string character*/
+	{
+		len++;
+		s++;
+	}
+	return (len);
 }
+
+/**
+ * _strcpy - copy the string pointed
+ * @dest: copy source here
+ * @src: this is tue source
+ * Return: returns copy of original
+*/
 
 char *_strcpy(char *dest, char *src)
 {

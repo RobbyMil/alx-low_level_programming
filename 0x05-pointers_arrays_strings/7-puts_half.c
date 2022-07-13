833a6fbacff551340d0d41a91e5bd22719284bc0
Robby Milambo <robbymilambo@gmail.com>
2022-07-13 04:14:09 +0200

    half string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * _strlen - compute lenght of string
+ * @s: string argument
+ * Return: lenght of argument s
+*/
+                                                           int _strlen(char *s)
+{
+        int len = 0;
+
+        while (*s != '\0')
+        {
+                len++;
+                s++;
+        }
+        return (len);
+}
+
+/**
+ * puts_half - print string in half
+ * @str: string argument
+*/
+
+void puts_half(char *str)
+{
+	int ltr;
+
+	int len = _strlen(str);
+
+	if (len % 2 != 0)
+	{
+		ltr = (len / 2) + 1;
+	else
+	{
+		ltr = (len / 2);
+	}
+	while (ltr < len)
+		putchar((*str + ltr));
+	ltr++;
+	}
+	putchar('\n');
+}
+
+
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,3 +8,4 @@ void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *strg);
+void puts_half(char *str);

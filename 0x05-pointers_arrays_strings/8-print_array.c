76fcfb4dd4f520ce6c3a17ead0f180e4c143658c
Robby Milambo <robbymilambo@gmail.com>
2022-07-13 05:12:32 +0200

    print array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+# include "main.h"
+
+/**
+ * print_array - prints elements of a list
+ * @a: array
+ * @n: elements of array
+*/
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i != (n - 1))
+		{
+			printf("%d ", a[i];
+		}
+		else
+		{
+			printf("%d", a[i];
+		}
+	}
+	printf("\n");
+}
+
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -9,3 +9,4 @@ void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *strg);
 void puts_half(char *str);
+void print_array(int *a, int n);

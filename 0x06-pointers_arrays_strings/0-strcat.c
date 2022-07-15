39a30574371f12174455e0506347f2456e9c109a
Robby Milambo <robbymilambo@gmail.com>
2022-07-15 16:05:40 +0200

    new directory

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strcat - performs concatenation of two strings
+ * @dest: first string
+ * @src: second string
+ * Return: returns concatenated string with empty string character appended
+*/
+
+char *_strcat(char *dest, char *src);
+{
+	int i = 0;
+
+	int j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[i] != '\0')
+	{
+		src[i] = dest[i];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}

97413247eabcead4adf15d99189225e08a77cb1c
Robby Milambo <robbymilambo@gmail.com>
2022-07-15 21:13:21 +0200

    hotfix

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse an array
+ * @a: array to reverse
+ * @n: total number of elements in array
+*/
+
+void reverse_array(int a, int n)
+{
+	int tmp, strt = 0;
+
+	int end = n - 1;
+
+	while(strt < end)
+	{
+		tmp = *(a + strt);
+		*(a + strt) = *(a + end);
+		*(a + end) = tmp;
+		strt++; end--;
+	}
+}

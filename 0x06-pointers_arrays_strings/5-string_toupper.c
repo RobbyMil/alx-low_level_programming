fd9853cbb82405cf1d5da5ba9e2f8fb6f1029413
Robby Milambo <robbymilambo@gmail.com>
2022-07-16 05:29:33 +0200

    capitalise string

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - ALL CAPS letters in a given strina
+ * @s: string argument
+ * Return: capitalized string
+*/
+
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	for(i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] -= 'a' + 'A';
+		}
+		
+		return (s)
+	}
